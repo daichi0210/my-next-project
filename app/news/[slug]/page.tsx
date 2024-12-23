f6f03e02237497b95718940db0721b3b3c1bcd2b
daichi <kabata0210@icloud.com>
2024-12-23 21:20:34 +0900

    ダイナミックルーティング

---
+++ app/news/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import { getNewsDetail } from "@/app/_libs/microcms";
+
+type Props = {
+  params: { slug: string };
+};
+
+export default async function Page({ params }: Props) {
+  const data = await getNewsDetail(params.slug);
+  return <div>{data.title}</div>;
+}
